02e7e88631c944cb1c1f16c6d58ef85b0bd50fcb
test <test@test.com>
2026-05-07 11:02:32 +0100

    thur

---
+++ Day 3/docker_practice_calc/calculator.py
@@ -0,0 +1,19 @@
+class Calculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def get_sum(self):
+        return self.a + self.b
+
+    def get_diff(self):
+        return self.a - self.b
+
+    def get_prod(self):
+        return self.a * self.b
+
+    def get_div(self):
+        return self.a / self.b
+
+myCalc = Calculator(a=3,b=5)
+print(myCalc.get_sum())
---
+++ Day 3/docker_practice_calc/test.py
@@ -0,0 +1,2 @@
+import sys
+print(f"Value: {sys.argv[1]}")
